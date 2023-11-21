049808e8c382eeea9f70aaa6b6b28ef320c00c8c
Annabella Riley <z5312567@ad.unsw.edu.au>
2023-11-22 08:41:31 +1100

    added tab view and login functionality

--- App.js
+++ App.js
@@ -2,18 +2,30 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { StartUp } from './StartUp.js';
+import { Main } from './Main.js';
 import { styles } from './Styles.js';
 
-
-
 export default function App() {
-  return (
-    <NavigationContainer styles={styles.site}>
-        <StartUp/>
-        <StatusBar style="auto" />
-    </NavigationContainer>
-  );
-}
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+    const login = () => {
+        setIsLoggedIn(true);
+    };
+
+    const logout = () => {
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <NavigationContainer styles={styles.site}>
+            {!isLoggedIn ? (
+                <StartUp login={login}/>
+            ) : (
+                <Main logout={logout}/>
+            )}
+            <StatusBar style="auto" />
+        </NavigationContainer>
+    );
+}
